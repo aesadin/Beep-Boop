8dfb2c6e39568f1040e155c72f8ca1b404a10764
Allison Sadin <aesadin@gmail.com>
2020-06-12 12:20:33 -0700

    creates function that creates a range and pushes that range to an array variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
 
+    const userNumber = paresInt($("#input").val());
 
-    function getInputNum() {
+
+    $(".result").show();
+    /*function getInputNum() {
       let userNumber = paresInt($("#input").val());
       return userNumber;
-    }
+    }*/
 
+//Business Logic
 
+for (i = 0; i < userNumber; i++) {
+  const numberArray = [];
+
+}
 
 
